5a6b856ff585833e02797ce26724b8ae3a4a40cb
Tom Eckstein <tom.eckstein@web.de>
2020-06-21 00:28:04 +0200

    Add Icon Filters

---
+++ LolQMaster/LolQMaster/Models/IconFilters.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LolQMaster.Models
+{
+    public class IconFilters
+    {
+        public IconFilters()
+        {
+
+        }
+
+        public IEnumerable<IconFilter> Filters { get => filters; }
+        private List<IconFilter> filters = new List<IconFilter>();
+
+        public void AddFilter(IconFilter iconFilter)
+        {
+            filters.Add(iconFilter);
+        }
+
+        public class IconFilter
+        {
+            public IconFilter(string name, IEnumerable<int> icons)
+            {
+                FilterName = name;
+                Icons = icons;
+            }
+
+            public string FilterName;
+            public IEnumerable<int> Icons;
+        }
+    }
+}
